e182d3afc4b8b296c822698ac5b44e0acae07bfc
traffick0 <62976585+traffick0@users.noreply.github.com>
2020-03-31 23:36:02 -0400

    Delete config.py

--- utils/config.py
+++
@@ -1,7 +0,0 @@
-story_from_users_followers = "false"
-story_from_feed = "false"
-follow_suggestions = "false"
-like = "true"
-comment = "true"
-report = "true"
-data_save_mode = "true" # set to true to no load any images (can increase performance and save data)
